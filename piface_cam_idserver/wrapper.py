5922eb90da233b7403380d6acce44c315de8c9ed
tensorfactory <yee.kwong.cheong@gmail.com>
2020-01-24 18:34:55 +0800

    Update version 1.3.0 to 1.3.4

--- piface_cam_idserver/wrapper.py
+++ piface_cam_idserver/wrapper.py
@@ -1,7 +1,8 @@
-import PiFaceCam_IDServer_1_3_0
+import PiFaceCam_IDServer_1_3_4
 import os
+
 
 if __name__ == "__main__":
  
-    PiFaceCam_IDServer_1_3_0.run_server()
+    PiFaceCam_IDServer_1_3_4.run_server()
     os.system("sudo shutdown -h now")
