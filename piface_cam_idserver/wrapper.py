ef7da38eabc70c823221e1eda06555e89ba82bb2
tensorfactory <yee.kwong.cheong@gmail.com>
2020-01-29 13:39:52 +0800

    Revision update

--- piface_cam_idserver/wrapper.py
+++ piface_cam_idserver/wrapper.py
@@ -1,8 +1,8 @@
-import PiFaceCam_IDServer_1_3_4
+import PiFaceCam_IDServer_1_3_5
 import os
 
 
 if __name__ == "__main__":
  
-    PiFaceCam_IDServer_1_3_4.run_server()
+    PiFaceCam_IDServer_1_3_5.run_server()
     os.system("sudo shutdown -h now")
--- pifacecam_client/wrapper_without_UDP_server.py
+++ pifacecam_client/wrapper_without_UDP_server.py
@@ -1,7 +1,7 @@
-import PiFaceCam_Client_1_3_5
+import PiFaceCam_Client_1_3_6
 import os
 
 if __name__ == "__main__":
 
-    PiFaceCam_Client_1_3_5.run_client("", -1)
+    PiFaceCam_Client_1_3_6.run_client("", -1)
     os.system("sudo shutdown -h now")
