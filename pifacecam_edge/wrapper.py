3f49a3148a510898630c4427ab0fc13a67a20d4f
tensorfactory <yee.kwong.cheong@gmail.com>
2020-03-05 18:30:35 +0800

    Upgrade everything to versio 2.0

--- pifacecam_edge/wrapper.py
+++ pifacecam_edge/wrapper.py
@@ -1,7 +1,7 @@
-import PiFaceCam_Edge_2_0_0
+import PiFaceCam_Edge_2_2_2
 import os
 
 if __name__ == "__main__":
 
-    PiFaceCam_Edge_2_0_0.run_client()
+    PiFaceCam_Edge_2_2_2.run()
     os.system("sudo shutdown -h now")
